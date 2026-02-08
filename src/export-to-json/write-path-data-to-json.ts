11be8060007988ea0134a2271f20b78a5d1789e8
Naved M. Usmani <navedm1424@gmail.com>
2026-02-08 18:56:53 +0530

    Alpha 6

--- src/export-to-json/write-path-data-to-json.ts
+++ src/export-to-json/write-path-data-to-json.ts
@@ -7,35 +7,36 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const targetDirectoryPath = path.resolve(__dirname, '../../json-exports');
+const DEFAULT_FILE_NAME = "sample";
 
-export async function writePathDataToJson(pathData: Path, outputFileName: string) {
+export async function writePathDataToJson(pathData: Path, outputDirectoryPath: string, outputFileName: string) {
     if (typeof process === "undefined" || !process.versions?.node) {
         throw new Error(`${writePathDataToJson.name} can only run in Node.js`);
     }
     if (!((pathData as any) instanceof Path))
         throw new Error("Invalid Path object.");
     if (typeof (outputFileName as any) !== "string")
-        outputFileName = "sample";
+        outputFileName = DEFAULT_FILE_NAME;
 
     const data = {
         pathData: pathData.toSVGPathString()
     };
 
     outputFileName = outputFileName.trim();
-    await mkdir(targetDirectoryPath, { recursive: true });
-    const filePath = path.resolve(targetDirectoryPath, `${outputFileName || "sample"}.json`);
+    const outputDirectory = path.resolve(__dirname, outputDirectoryPath);
+    await mkdir(outputDirectory, { recursive: true });
+    const filePath = path.resolve(outputDirectoryPath, `${outputFileName || DEFAULT_FILE_NAME}.json`);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
 
     return filePath;
 }
 
-export async function writePathFramesToJson(durationInSeconds: number, paths: Path[], outputFileName: string) {
+export async function writePathFramesToJson(durationInSeconds: number, paths: Path[], outputDirectoryPath: string, outputFileName: string) {
     if (typeof process === "undefined" || !process.versions?.node) {
         throw new Error(`${writePathFramesToJson.name} can only run in Node.js`);
     }
     if (typeof (outputFileName as any) !== "string")
-        outputFileName = "sample";
+        outputFileName = DEFAULT_FILE_NAME;
 
     const data = {
         durationMs: durationInSeconds * 1000,
@@ -47,8 +48,9 @@ export async function writePathFramesToJson(durationInSeconds: number, paths: Pa
     };
 
     outputFileName = outputFileName.trim();
-    await mkdir(targetDirectoryPath, { recursive: true });
-    const filePath = path.resolve(targetDirectoryPath, `${outputFileName || "sample"}.json`);
+    const outputDirectory = path.resolve(__dirname, outputDirectoryPath);
+    await mkdir(outputDirectory, { recursive: true });
+    const filePath = path.resolve(outputDirectory, `${outputFileName || DEFAULT_FILE_NAME}.json`);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
 
     return filePath;
