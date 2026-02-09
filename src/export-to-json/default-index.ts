35d1fdb76ca520b5c09e6d08d3b698b4057edc4e
Naved M. Usmani <navedm1424@gmail.com>
2026-02-09 16:06:49 +0530

    Alpha 6

--- src/export-to-json/default-index.ts
+++ src/export-to-json/default-index.ts
@@ -1,9 +1,9 @@
 import type {Path} from "../path";
 
-export function writePathDataToJson(pathData: Path, outputFileName: string)  {
+export function writePathDataToJson(pathData: Path, outputDirectoryPath: string, outputFileName: string)  {
     throw new Error(`${writePathDataToJson.name} can only run in Node.js`);
 }
 
-export function writePathFramesToJson(duration: number, paths: Path[], outputFileName: string) {
+export function writePathFramesToJson(durationInSeconds: number, paths: Path[], outputDirectoryPath: string, outputFileName: string) {
     throw new Error(`${writePathFramesToJson.name} can only run in Node.js`);
 }
--- src/export-to-json/write-path-data-to-json.ts
+++ src/export-to-json/write-path-data-to-json.ts
@@ -2,38 +2,24 @@ import fs from "fs";
 import {mkdir} from "fs/promises";
 import path from "path";
 import {Path} from "../path";
-import {fileURLToPath} from "node:url";
 
 const DEFAULT_FILE_NAME = "sample";
 
-function getInvokerPath() {
-    const originalPrepareStackTrace = Error.prepareStackTrace;
-    Error.prepareStackTrace = (_, stack) => stack;
-    const err = new Error();
-    const stack = err.stack as unknown as NodeJS.CallSite[];
-    Error.prepareStackTrace = originalPrepareStackTrace;
-
-    // stack[0] is this function, stack[1] is the caller
-    const callerFile = stack[2]!.getFileName();
-    return fileURLToPath(callerFile!);
-}
-
 export async function writePathDataToJson(pathData: Path, outputDirectoryPath: string, outputFileName: string) {
-    if (typeof process === "undefined" || !process.versions?.node) {
+    if (typeof process === "undefined" || !process.versions?.node)
         throw new Error(`${writePathDataToJson.name} can only run in Node.js`);
-    }
     if (!((pathData as any) instanceof Path))
         throw new Error("Invalid Path object.");
     if (typeof (outputFileName as any) !== "string")
         outputFileName = DEFAULT_FILE_NAME;
-    const __dirname = path.dirname(getInvokerPath());
+    const cwd = process.cwd();
 
     const data = {
         pathData: pathData.toSVGPathString()
     };
 
     outputFileName = outputFileName.trim();
-    const outputDirectory = path.resolve(__dirname, outputDirectoryPath);
+    const outputDirectory = path.resolve(cwd, outputDirectoryPath);
     await mkdir(outputDirectory, { recursive: true });
     const filePath = path.resolve(outputDirectoryPath, `${outputFileName || DEFAULT_FILE_NAME}.json`);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
@@ -42,9 +28,8 @@ export async function writePathDataToJson(pathData: Path, outputDirectoryPath: s
 }
 
 export async function writePathFramesToJson(durationInSeconds: number, paths: Path[], outputDirectoryPath: string, outputFileName: string) {
-    if (typeof process === "undefined" || !process.versions?.node) {
+    if (typeof process === "undefined" || !process.versions?.node)
         throw new Error(`${writePathFramesToJson.name} can only run in Node.js`);
-    }
     if (typeof (outputFileName as any) !== "string")
         outputFileName = DEFAULT_FILE_NAME;
 
@@ -56,10 +41,10 @@ export async function writePathFramesToJson(durationInSeconds: number, paths: Pa
             throw new Error("Invalid Path object.");
         })
     };
-    const __dirname = path.dirname(getInvokerPath());
+    const cwd = process.cwd();
 
     outputFileName = outputFileName.trim();
-    const outputDirectory = path.resolve(__dirname, outputDirectoryPath);
+    const outputDirectory = path.resolve(cwd, outputDirectoryPath);
     await mkdir(outputDirectory, { recursive: true });
     const filePath = path.resolve(outputDirectory, `${outputFileName || DEFAULT_FILE_NAME}.json`);
     fs.writeFileSync(filePath, JSON.stringify(data, null, 2), "utf8");
